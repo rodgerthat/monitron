0d0b384a853ac4186d75c3877be995eb6b43cf87
Joel Norris <joel.r.norris@gmail.com>
2018-03-08 12:03:31 -0600

    basic functionality complete

--- Controllers/HeatController.py
+++ Controllers/HeatController.py
@@ -1,34 +1,49 @@
-
+#
 
 import RPi.GPIO as GPIO
 
 
-class HeatController():
+class HeatController:
 
     heatON = True   # Heater is plugged into 'Normally ON' outlet in IoT Relay
                     # this is so if there's any kind of reset or power outage it will
                     # come on by default
 
-    def __init__(self):
+    outletType = 'NormallyON'
+
+    def __init__(self, outlet_type):
+
+        self.outletType = outlet_type
 
         # prime the device!
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(18, GPIO.OUT)
-        GPIO.output(18, GPIO.LOW)   # the IoT relay initially starts in the OFF position
-        self.heatON = False;
+
+        if self.outletType == 'NormallyON':
+            GPIO.output(18, GPIO.HIGH)   # the IoT relay initially starts in the OFF position
+
+        elif self.outletType == 'NormallyOFF':
+            GPIO.output(18, GPIO.LOW)   # the IoT relay initially starts in the OFF position
 
     def turn_ON(self):
 
         # turn on the HEAT
-        GPIO.output(18, GPIO.HIGH)
+        if self.outletType == 'NormallyON':
+            GPIO.output(18, GPIO.LOW)
+
+        elif self.outletType == 'NormallyOFF':
+            GPIO.output(18, GPIO.HIGH)
+
         self.heatON = True
 
     def turn_OFF(self):
 
         # turn off the HEAT (brow brow kiddies!)
-        GPIO.output(18, GPIO.LOW)
+        if self.outletType == 'NormallyON':
+            GPIO.output(18, GPIO.HIGH)
+
+        if self.outletType == 'NormallyOFF':
+            GPIO.output(18, GPIO.LOW)
+
         self.heatON = False
-
-
-
--- Monitron.py
+++ Monitron.py
@@ -19,66 +19,84 @@ class Monitron:
     tempStorer = object
     heatController = object
 
-    heatON = False
-
     def __init__(self):
 
         # initialize
         self.tempGetter = TempGetter()
-        #self.tempStorer = TempStorer()
-        self.heatController = HeatController()
+        # self.tempStorer = TempStorer()
+        self.heatController = HeatController('NormallyON')  # Tell the controller which plug
 
-    def monitor(self, tempMin, tempMax, timeInterval):
+    def print_status(self):
+
+        # print "%s %s" % (hello, world)
+        # print("{} {}".format(hello, world))
+        print("Temp : {} | heatON : {}".format(self.currentTemp, self.heatController.heatON))
+        print("-----------------------------------------")
+
+    def monitor(self, temp_min, temp_max, time_interval):
 
         while True:
+
+            # see what's up
 
             # # get the current temperature in Raw Temp Data, direct from the
             # self.currentTemp = self.tempGetter.get_temp('R')
             # print(self.currentTemp)
             #
-            # # get the current temperature in Celcius
+            # # get the current temperature in Celsius
             # self.currentTemp = self.tempGetter.get_temp('C')
             # print(self.currentTemp)
             #
-            # # get the current temperature in Farenheight
+            # # get the current temperature in Fahrenheit
             # self.currentTemp = self.tempGetter.get_temp('F')
             # print(self.currentTemp)
 
             # test the HeatController
-            # if self.heatController.heatON == False:
-            #
-            #     self.heatController.turn_ON()
-            #
-            # else:
+            # if it's on, turn it off,
+            # if it's off, turn it on.
+            # if self.heatController.heatON:
             #
             #     self.heatController.turn_OFF()
+            #
+            # elif not self.heatController.heatON:
+            #
+            #     self.heatController.turn_ON()
 
-            # get the current temperature in Farenheight
+            # else:
+            #
+            #     self.heatController.turn_ON()
+
+            # get the current temperature in Fahrenheit
             self.currentTemp = self.tempGetter.get_temp('F')
-            print(self.currentTemp)
 
             # if the current temp is lower then the lowest temp minimum we want,
-            if self.currentTemp < tempMin:
+            if self.currentTemp < temp_min:
+
+                print("currentTemp : {} < temp_min : {}".format(self.currentTemp, temp_min))
 
                 # turn heat on
                 self.heatController.turn_ON()
-                print(self.heatController.heatON)
 
             # else if the current temp is greater than the max temp we want,
-            elif self.currentTemp > tempMax:
+            elif self.currentTemp > temp_max:
 
-                # turn heat off
+                print("currentTemp : {} < temp_max : {}".format(self.currentTemp, temp_min))
+
+                # turn heat off, if it isnt' already off
                 self.heatController.turn_OFF()
-                print(self.heatController.heatON)
+
+            # checkitty check yo sef b4 u wreck yo sef
+            self.print_status()
 
             # give it a rest. don't spam the sensor too much
-            time.sleep(timeInterval)
+            time.sleep(time_interval)
 
 
 def main():
 
     monitron = Monitron()
-    #monitron.monitor(80, 90, 5)
-    monitron.monitor(80, 83, 1)
+    # monitron.monitor(80, 90, 3)   # just for heat controller test
+    monitron.monitor(80, 90, 1)
+
 
 main()
